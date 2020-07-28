9483b773f00361a8bc1df724ca6bb657c18c782b
shareed <shanreed6@gmail.com>
2020-07-28 00:49:24 -0700

    added client side authorization

---
+++ AdvanceWebApplications/clientsideauthentication-app/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Login from './components/Login';
+import GasPrices from './components/GasPrices';
+import PrivateRoute from './components/PrivateRoute';
+import './App.css'
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          {/* Protected Route */}
+          <PrivateRoute exact path="/protected" component={GasPrices} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
